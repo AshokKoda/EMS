492f6277e398b043d2d99c7351a154157fd7f843
Koda Ashok <a.koda59@gmail.com>
2023-08-08 22:01:38 +0530

    Now any mandatory field is empty display message

--- src/main/java/com/example/employee/Services/EmployeeService.java
+++ src/main/java/com/example/employee/Services/EmployeeService.java
@@ -13,7 +13,10 @@ import org.springframework.stereotype.Service;
 import com.example.employee.Entities.Employee;
 import com.example.employee.Repositories.EmployeeRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class EmployeeService {
 
     @Autowired
@@ -21,15 +24,27 @@ public class EmployeeService {
 
     // SAVE EMPLOYEE
     public Employee save(Employee employee) throws ParseException {
-        String strDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        Date doj = df.parse(strDate);
-        employee.setDoj(doj);
-        return employeeRepository.save(employee);
+        String fName = employee.getFirstName();
+        String lName = employee.getLastName();
+        String email = employee.getEmail();
+        String phoneNo = employee.getPhoneNo();
+        Double salary = employee.getSalary();
+
+        if (fName.isEmpty() || lName.isEmpty() || email.isEmpty() || phoneNo.isEmpty() || salary <= 0) {
+            log.info("Some mandatory fields are missing!.");
+        } else {
+            String strDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+            Date doj = df.parse(strDate);
+            employee.setDoj(doj);
+            employeeRepository.save(employee);
+            log.info("Employee successfully added!.");
+        }
+        return employee;
     }
 
     // GET EMPLOYEES
     public List<Employee> getAllEmployees() {
-		return employeeRepository.findAll();
-	}
+        return employeeRepository.findAll();
+    }
 }
